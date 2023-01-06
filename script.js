c8b95be1fd11345b8e1f7a2c914d4b3e5b91d438
gitkap13 <116759485+gitkap13@users.noreply.github.com>
2023-01-05 19:32:35 -0500

    Add function to run 5 rounds of the game and return total score

--- script.js
+++ script.js
@@ -12,41 +12,63 @@ function getComputerChoice() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    let winningMessage = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`
-    let losingMessage = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`
-    if (playerSelection === computerSelection) {
-        return "It's a tie!"
+
+
+function playGame() {
+    let winCount = 0
+    let loseCount = 0
+    let drawCount = 0
+
+    function playRound(playerSelection, computerSelection) {
+        playerSelection = prompt("Rock, paper, or scissors?")
+        computerSelection = getComputerChoice()
+        let drawMessage = "It's a draw!"
+        let winningMessage = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`
+        let losingMessage = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`
+        if (playerSelection === computerSelection) {
+            ++drawCount;
+            return drawMessage
+        }
+        else if (playerSelection === "rock"){
+            if (computerSelection === "paper") {
+                ++loseCount;
+                return losingMessage
+            }
+            else if (computerSelection === "scissors") {
+                ++winCount;
+                return winningMessage
+            }
+        }
+        else if (playerSelection === "paper") {
+            if (computerSelection === "rock") {
+                ++winCount;
+                return winningMessage
+            }
+            else if (computerSelection === "scissors") {
+                ++loseCount;
+                return losingMessage
+            }
+        }
+        else if (playerSelection === "scissors") {
+            if (computerSelection === "paper") {
+                ++winCount;
+                return winningMessage
+            }
+            else if (computerSelection === "rock") {
+                ++loseCount;
+                return losingMessage
+            }
+        }
+        else {
+            return "Invalid input. Please try again."
+        }
     }
-    else if (playerSelection === "rock"){
-        if (computerSelection === "paper") {
-            return losingMessage
-        }
-        else if (computerSelection === "scissors") {
-            return winningMessage
-        }
-    }
-    else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return winningMessage
-        }
-        else if (computerSelection === "scissors") {
-            return losingMessage
-        }
-    }
-    else if (playerSelection === "scissors") {
-        if (computerSelection === "paper") {
-            return winningMessage
-        }
-        else if (computerSelection === "rock") {
-            return losingMessage
-        }
-    }
-    else {
-        return "Invalid input. Please try again."
-    }
+
+    for (let i = 0; i < 5; i++) { 
+        console.log(playRound())
+    };
+
+    return `Final score: You: ${winCount} Computer: ${loseCount} Draw: ${drawCount}`
 }
-let computerSelection = getComputerChoice()
-let playerSelection = "paPeR"
-console.log(playRound(playerSelection, computerSelection))
+
+console.log(playGame())
