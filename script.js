9abd620ee670b3174d95e3355b4242106b002f44
gitkap13 <116759485+gitkap13@users.noreply.github.com>
2023-01-10 14:45:45 -0500

    Clean up code and improve readability

--- script.js
+++ script.js
@@ -2,22 +2,16 @@
 let winCount = 0
 let loseCount = 0
 let drawCount = 0
+let final_result = ""
+
 const buttons = document.querySelector('#buttons')
 const results = document.querySelector('#results')
 const score = document.querySelector('#score')
 score.textContent = `Wins: ${winCount}, Losses: ${loseCount}, Draws: ${drawCount}`
 
 function getComputerChoice() {
-    let getIntValue = Math.floor(Math.random() * 3) + 1
-    if (getIntValue === 1) {
-        return "rock"
-    }
-    else if (getIntValue === 2) {
-        return "paper"
-    }
-    else if (getIntValue === 3) {
-        return "scissors"
-    }
+    let choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(Math.random()*choices.length)]
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -25,58 +19,41 @@ function playRound(playerSelection, computerSelection) {
     let drawMessage = "It's a draw!"
     let winningMessage = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection}.`
     let losingMessage = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}.`
-    let final_result = ""
-    if (winCount => 5) {
-        final_result = "You win!"
-    }
-    else if (loseCount => 5) {
-        final_result = "You lose!"
-    }
+
     if (winCount < 5 && loseCount < 5) {
         if (playerSelection === computerSelection) {
             ++drawCount;
             return drawMessage
         }
-        else if (playerSelection === "rock"){
-            if (computerSelection === "paper") {
-                ++loseCount;
-                return losingMessage
-            }
-            else if (computerSelection === "scissors") {
-                ++winCount;
-                return winningMessage
-            }
-        }
-        else if (playerSelection === "paper") {
-            if (computerSelection === "rock") {
-                ++winCount;
-                return winningMessage
-            }
-            else if (computerSelection === "scissors") {
-                ++loseCount;
-                return losingMessage
-            }
-        }
-        else if (playerSelection === "scissors") {
-            if (computerSelection === "paper") {
-                ++winCount;
-                return winningMessage
-            }
-            else if (computerSelection === "rock") {
-                ++loseCount;
-                return losingMessage
-            }
-        }
+        else if ((playerSelection === "rock" && computerSelection === "paper") ||
+                (playerSelection === "paper" && computerSelection === "scissors") ||
+                (playerSelection === "scissors" && computerSelection === "rock")) {
+                    ++loseCount
+                    return losingMessage
+                }
+        else if ((playerSelection === "paper" && computerSelection === "rock") ||
+                (playerSelection === "rock" && computerSelection === "scissors") ||
+                (playerSelection === "scissors" && computerSelection === "paper")) {
+                    ++winCount
+                    return winningMessage
+                }
         else {
             return "Invalid input. Please try again."
         }
-    }
-    else {
-        return `Game over! ${final_result}`
     }
 }
 
 buttons.addEventListener('click', function(e) {
     results.textContent = playRound(e.target.id);
     score.textContent = `Wins: ${winCount}, Losses: ${loseCount}, Draws: ${drawCount}`
+    if (winCount >= 5) {
+        final_result = "You win!"
+        results.textContent = `Game over! ${final_result}`
+        buttons.removeEventListener()
+    }
+    else if (loseCount >= 5) {
+        final_result = "You lose!"
+        results.textContent = `Game over! ${final_result}`
+        buttons.removeEventListener()
+    }
 })
